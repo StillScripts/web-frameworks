2c9ab4b7c867b87ae9fc0454aed1fabec34bc022
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-08-05 21:51:14 +1000

    Create Layout component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,34 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Layout from "../src/layout/Layout";
+import {
+  BasicVariables,
+  ConditionalRendering,
+  DependentVariables,
+  HelloWorld,
+  HTML,
+  ListLooping,
+  NestingComponents,
+  Props,
+  Styling,
+} from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+// An enum containing an option for each component example
+export enum Components {
+  HelloWorld,
+  Styling,
+  NestingComponents,
+  Props,
+  HTML,
+  BasicVariables,
+  DependentVariables,
+  ConditionalRendering,
+  ListLooping,
+  TypingAnimation,
 }
 
-export default App
+// A list of each component based on the enum options above
+export const componentsList: Components[] = [
+  Components.HelloWorld,
+  Components.Styling,
+  Components.NestingComponents,
+  Components.Props,
+  Components.HTML,
+  Components.BasicVariables,
+  Components.DependentVariables,
+  Components.ConditionalRendering,
+  Components.ListLooping,
+  Components.TypingAnimation,
+];
+
+/**
+ * Method which converts an option of the Components enum into
+ * a string to display a title for a component.
+ * @param {Components} component
+ * @returns {string}
+ */
+export const getComponentTitle = (component: Components): string => {
+  switch (component) {
+    case Components.HelloWorld:
+      return "Hello World";
+    case Components.Styling:
+      return "Styling";
+    case Components.NestingComponents:
+      return "Nesting Components";
+    case Components.Props:
+      return "Passing Props";
+    case Components.HTML:
+      return "HTML Tags";
+    case Components.BasicVariables:
+      return "Basic Variables";
+    case Components.DependentVariables:
+      return "Dependent Variables";
+    case Components.ConditionalRendering:
+      return "Conditional Rendering";
+    case Components.ListLooping:
+      return "List Looping";
+    case Components.TypingAnimation:
+      return "Typing Animation";
+  }
+};
+
+const Home = () => {
+  const [currentComponent, setCurrentComponent] = useState<Components>(
+    Components.HelloWorld
+  );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleSwitch = (newComponent?: Components) => {
+    typeof newComponent === "number" && setCurrentComponent(newComponent);
+    setMenuOpen(false);
+  };
+
+  return (
+    <Layout
+      currentComponent={currentComponent}
+      action={() => setMenuOpen(false)}
+      handleSwitch={handleSwitch}
+      menuOpen={menuOpen}
+    >
+      <div>
+        {currentComponent === Components.HelloWorld ? (
+          <HelloWorld />
+        ) : currentComponent === Components.Styling ? (
+          <Styling />
+        ) : currentComponent === Components.NestingComponents ? (
+          <NestingComponents />
+        ) : currentComponent === Components.Props ? (
+          <Props />
+        ) : currentComponent === Components.HTML ? (
+          <HTML />
+        ) : currentComponent === Components.BasicVariables ? (
+          <BasicVariables />
+        ) : currentComponent === Components.DependentVariables ? (
+          <DependentVariables />
+        ) : currentComponent === Components.ConditionalRendering ? (
+          <ConditionalRendering />
+        ) : currentComponent === Components.ListLooping ? (
+          <ListLooping />
+        ) : (
+          "empty..."
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
