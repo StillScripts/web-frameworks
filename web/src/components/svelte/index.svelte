72ff3b74d376da54cf7b989745fb83c1edbd3515
StillScripts <80142739+StillScripts@users.noreply.github.com>
2022-10-14 21:31:17 +1100

    Add page for Svelte components

---
+++ web/src/components/svelte/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Components } from "../../utils/components";
+  import BasicVariables from "./BasicVariables.svelte";
+  import ConditionalRendering from "./ConditionalRendering.svelte";
+  import DependentVariables from "./DependentVariables.svelte";
+  import HelloWorld from "./HelloWorld.svelte";
+  import HTML from "./HTML.svelte";
+  import ListLooping from "./ListLooping.svelte";
+  import PassingProps from "./PassingProps.svelte";
+  import Styling from "./Styling.svelte";
+
+  export let currentComponent: Components = Components.HelloWorld;
+</script>
+
+<div>
+  {#if currentComponent === Components.HelloWorld}
+    <HelloWorld />
+  {:else if currentComponent === Components.Styling}
+    <Styling />
+  {:else if currentComponent === Components.Props}
+    <PassingProps />
+  {:else if currentComponent === Components.HTML}
+    <HTML />
+  {:else if currentComponent === Components.BasicVariables}
+    <BasicVariables />
+  {:else if currentComponent === Components.DependentVariables}
+    <DependentVariables />
+  {:else if currentComponent === Components.ConditionalRendering}
+    <ConditionalRendering />
+  {:else if currentComponent === Components.ListLooping}
+    <ListLooping />
+  {/if}
+</div>
---
+++ web/src/utils/components.ts
@@ -0,0 +1,54 @@
+export enum Components {
+  HelloWorld,
+  Styling,
+  Props,
+  HTML,
+  BasicVariables,
+  DependentVariables,
+  ConditionalRendering,
+  ListLooping,
+  TypingAnimation,
+}
+
+// A list of each component based on the enum options above
+export const componentsList: Components[] = [
+  Components.HelloWorld,
+  Components.Styling,
+  Components.Props,
+  Components.HTML,
+  Components.BasicVariables,
+  Components.DependentVariables,
+  Components.ConditionalRendering,
+  Components.ListLooping,
+];
+
+/**
+ * Method which converts an option of the Components enum into
+ * a string to display a title for a component.
+ * @param {Components} component
+ * @returns {string}
+ */
+export const getComponentTitle = (component: Components): string => {
+  switch (component) {
+    case Components.HelloWorld:
+      return "Hello World";
+    case Components.Styling:
+      return "Styling";
+    case Components.Props:
+      return "Passing Props";
+    case Components.HTML:
+      return "HTML Tags";
+    case Components.BasicVariables:
+      return "Basic Variables";
+    case Components.DependentVariables:
+      return "Dependent Variables";
+    case Components.ConditionalRendering:
+      return "Conditional Rendering";
+    case Components.ListLooping:
+      return "List Looping";
+    case Components.TypingAnimation:
+      return "Typing Animation";
+    default:
+      return "Hello World";
+  }
+};
